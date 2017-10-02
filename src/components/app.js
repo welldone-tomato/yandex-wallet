456b02bb53af3d055e4f369d9f32f05db4c3aa3d
kroniak <me@kroniak.net>
2017-10-02 05:21:44 +0300

    [edit] ui changes

--- src/components/app.js
+++ src/components/app.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
 
-import Nav from './nav';
+import Nav from './nav/';
 
 class App extends Component {
   render() {
     return (
-      <div className="container">
+      <div>
         <Nav />
         <div className="section">
           { this.props.children }
--- src/components/cards/index.js
+++ src/components/cards/index.js
@@ -45,19 +45,29 @@ class Cards extends Component {
   }
 
   renderCard = cards => {
+    const st = {
+      minWidth: '466px',
+      maxWidth: '466px',
+      cursor: 'pointer'
+    }
+
     const cardList = cards.map(card => {
       return (
-        <div className="card-credit card--front">
-          <div className="card__number">
-            { this.formatCardNumber(card.cardNumber) }
+        <div className="tile is-child box" key={ card.id } style={ st }>
+          <div class="content">
+            <div className="card-credit card--front">
+              <div className="card__number">
+                { this.formatCardNumber(card.cardNumber) }
+              </div>
+              <div className="card__expiry-date">
+                { card.exp }
+              </div>
+              <div className="card__owner">
+                { card.name }
+              </div>
+              { this.returnLogo(card) }
+            </div>
           </div>
-          <div className="card__expiry-date">
-            { card.exp }
-          </div>
-          <div className="card__owner">
-            { card.name }
-          </div>
-          { this.returnLogo(card) }
         </div>
       )
     });
@@ -67,11 +77,30 @@ class Cards extends Component {
 
   render() {
     return (
-      <div className="columns is-mobile">
-        <div className="column is-half">
-          { this.renderCard(this.props.cards) }
-        </div>
-        <div className="column"></div>
+      <div className="container">
+        <h1 className="title">Список ваших карт</h1>
+        <h2 className="subtitle">Список карт, транзакций по ним и добавление операций</h2>
+        <hr/>
+        { this.props.error && <div className="notification is-danger">
+                                <button className="delete"></button>Произошла проблема при получении данных. Попробуйте еще раз через некоторое время.
+                              </div> }
+        { this.props.cards && <div className="tile is-ancestor">
+                                <div className="tile is-6 is-vertical is-parent">
+                                  { this.renderCard(this.props.cards) }
+                                </div>
+                                <div className="tile is-parent">
+                                  <div className="tile is-child box">
+                                    <p className="title">Three</p>
+                                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam semper diam at erat pulvinar, at pulvinar felis blandit. Vestibulum volutpat tellus diam, consequat
+                                      gravida libero rhoncus ut. Morbi maximus, leo sit amet vehicula eleifend, nunc dui porta orci, quis semper odio felis ut quam.</p>
+                                    <p>Suspendisse varius ligula in molestie lacinia. Maecenas varius eget ligula a sagittis. Pellentesque interdum, nisl nec interdum maximus, augue diam porttitor
+                                      lorem, et sollicitudin felis neque sit amet erat. Maecenas imperdiet felis nisi, fringilla luctus felis hendrerit sit amet. Aenean vitae gravida diam, finibus
+                                      dignissim turpis. Sed eget varius ligula, at volutpat tortor.</p>
+                                    <p>Integer sollicitudin, tortor a mattis commodo, velit urna rhoncus erat, vitae congue lectus dolor consequat libero. Donec leo ligula, maximus et pellentesque
+                                      sed, gravida a metus. Cras ullamcorper a nunc ac porta. Aliquam ut aliquet lacus, quis faucibus libero. Quisque non semper leo.</p>
+                                  </div>
+                                </div>
+                              </div> }
       </div>
       );
   }
