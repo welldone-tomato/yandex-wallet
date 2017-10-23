c84d0acd451f4d8f25c18fb797396a416d43ca87
Nikolay Molchanov <me@kroniak.net>
2017-10-24 01:55:20 +0300

    [merge] crt from vladimir

--- ssl/generate.sh
+++ ssl/generate.sh
@@ -1,10 +1,21 @@
 #!/bin/bash
 
-# Генерируем секретный ключ
-openssl genrsa -out key.pem 2048
+KEYSTORE_DIR=crt
 
-# Создаем запрос на подпись сертификата
-openssl req -new -sha256 -key key.pem -out csr.pem
+if [[ -f $KEYSTORE_DIR/key.pem || -f $KEYSTORE_DIR/cert.pem ]]; then
+       printf "Keys already exists. Exiting."
+       exit 0
+fi
 
-# Создаем "самоподписанный" публичный ключ
-openssl x509 -req -in csr.pem -signkey key.pem -out cert.pem
+# Create directory
+rm -rf $KEYSTORE_DIR
+mkdir $KEYSTORE_DIR
+
+
+# Generate key and certificate
+openssl genrsa -out $KEYSTORE_DIR/key.key 2048
+openssl req -new -sha256 -key $KEYSTORE_DIR/key.key -days 3650 -out $KEYSTORE_DIR/csr.csr
+openssl x509 -req -days 3650 -in $KEYSTORE_DIR/csr.csr -signkey $KEYSTORE_DIR/key.key -out $KEYSTORE_DIR/cert.crt
+
+# Clean CSR
+rm -f $KEYSTORE_DIR/csr.csr
