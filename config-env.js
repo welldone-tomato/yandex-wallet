6556668727df82000d592fe9ac4a06f7c1319769
Gleb Vorontsov <gleb.vorontsov@datart.com>
2017-10-30 12:46:09 +0300

    JWT secret key as env variable

--- config-env.js
+++ config-env.js
@@ -1,6 +1,6 @@
 /*** Define CONST */
 const MONGO = process.env.NODE_MONGO || 'mongodb://localhost/wallet';
-const JWT_SECRET = 'jjskskKKKAjajenwfpaP';
+const JWT_SECRET = process.env.NODE_JWT_SECRET  || 'jjskskKKKAjajenwfpaP';
 
 module.exports = {
     MONGO,
