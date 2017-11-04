7f5b1e78a76d8aebf64ad1eb3ffb0c949f1d03c3
dazzlegit <dronicl@yandex.ru>
2017-11-04 21:18:16 +0300

    [websockets] server: middleware refactoring + edit package.json

--- server/websockets/middleware.js
+++ server/websockets/middleware.js
@@ -4,7 +4,12 @@ module.exports = async (ctx, next) => {
   await next();
   if (ctx.broadcastCardIds) {
     const { userId } = ctx.params;
-    const message = JSON.stringify(ctx.broadcastCardIds);
+    
+    const message = JSON.stringify({
+      type: 'CARD_IDS',
+      data: ctx.broadcastCardIds,
+    });
+    
     SocketManager.broadcast(userId, message);
   }
 };
