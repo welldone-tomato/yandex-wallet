399bb028dd4df9c81ff3d9e1a94e936cd9a0017c
welldone-tomato <finder.success@yandex.ru>
2017-11-05 21:18:42 +0300

    added notifications for new events through WS

---
+++ src/services/notifications.js
@@ -0,0 +1,50 @@
+export default ({
+ 	_favicon_origin: '/favicon.ico',
+ 	_favicon_event: '/favicon_event.ico',
+ 	_title_origin: 'page',
+	init: function(title, src) {
+		this._title_origin = title || this._title_origin;
+		this._favicon_origin = src || this._favicon_origin;
+	},
+	sendNotification: function(title, src) {
+		const self = this;
+		self.changeFavicon(src || self._favicon_event);
+		self.changeTitle(title);
+		window.onfocus = null;
+		window.onfocus = ()=>{
+			setTimeout(()=>{
+				self.changeFavicon(self._favicon_origin);
+				self.changeTitle(self._title_origin);
+			}, 1000);
+			
+		};
+	},
+	changeFavicon: function (src) {
+		/*!
+		 * Dynamically changing favicons with JavaScript
+		 * Works in all A-grade browsers except Safari and Internet Explorer
+		 * Demo: http://mathiasbynens.be/demo/dynamic-favicons
+		 */
+
+		// HTML5™, baby! http://mathiasbynens.be/notes/document-head
+		const header = document.head || document.getElementsByTagName('head')[0];
+
+		let link = document.createElement('link');
+		let oldLink = document.getElementById('dynamic-favicon');
+		link.id = 'dynamic-favicon';
+		link.rel = 'shortcut icon';
+		link.href = src;
+		if (oldLink) {
+			header.removeChild(oldLink);
+		}
+		header.appendChild(link);
+	},
+	changeTitle: function (title) {
+		document.title = title;	
+	}
+});
+	
+
+	
+
+	
