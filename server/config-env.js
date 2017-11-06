b84ec88b03c44deeabb042e6b6740535ed3109ce
Kroniak <me@kroniak.net>
2017-11-06 22:12:30 +0300

    Update config-env.js

--- server/config-env.js
+++ server/config-env.js
@@ -1,7 +1,6 @@
 /*** Define CONST */
 
-// const MONGO = process.env.NODE_MONGO || 'mongodb://docker/test_yandex_wallet';
-const MONGO = process.env.NODE_MONGO || 'mongodb://localhost:27017/test_wallet';
+const MONGO = process.env.NODE_MONGO || 'mongodb://docker/test_yandex_wallet';
 const JWT_SECRET = process.env.NODE_JWT_SECRET || 'jjskskKKKAjajenwfpaP';
 const DOMAIN = process.env.NODE_DOMAIN || 'wallet.kroniak.net';
 const PORT = process.env.NODE_PORT || 4000;
